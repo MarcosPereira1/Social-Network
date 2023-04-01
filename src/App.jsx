75030e5f08d33e96a4443f3f7a59419b5f07dcef
marcospereira01 <marcospereira2023@hotmail.com>
2023-04-01 19:38:30 -0400

    add components css

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import { Header } from './components/Header';
+import './styles.css';
 
 function App() {
 
   return (
-<h1>Hello World</h1>
+  <div>
+    <Header />
+    <h1>Hello w</h1>
+  </div>
   )
 }
 
